98d5908b45478271dce1be3bc5bab0a9515a83dc
YunlongNie <yunlong.nie@yahoo.ca>
2016-06-02 15:14:56 -0700

    add prediction set

--- Demo/demon.R
+++ Demo/demon.R
@@ -7,7 +7,7 @@ library(sFPCA)
 data(medfly)
 xmat = medfly$eggcount  # 26 days and 50 flies 
 y = medfly$lifetime
-res = sfpca(xmat,y,0.1,1e3,npc_select=5)
+res = sfpca(xmat,y,0.1,1e3,npc_select=5,xmat_new = xmat[,1:10])
 plot(res$beta_fd,ylab="beta(t)")
 res$fitted 
 plot(res$sfpcs[[1]], main="the first sFPC")
@@ -16,7 +16,7 @@ plot(res$sfpcs[[1]], main="the first sFPC")
 
 # a binary response case 
 data(binary_dat);xmat = binary_dat$x;y=binary_dat$y
-res = sfpcs_binary(xmat,y,npc=2,theta=1)
+res = sfpcs_binary(xmat,y,npc_select=2,theta=1,xmat_new = xmat)
 res$fitted
 mean(y==res$fitted)
 res$sfpcs
