067602246a6d9a9026dfeb4d5cd01ae59f13cd08
YunlongNie <yunlong.nie@stat.ubc.ca>
2017-06-15 14:20:28 -0700

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,25 @@
+.onLoad <- function(libname, pkgname) 
+{
+  library.dynam("sFPCA", pkgname, libname)
+}
+
+.onAttach <- function(lib, pkg)
+{
+  unlockBinding(".sFPCA", asNamespace("sFPCA")) 
+  version <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
+  
+  if(interactive())
+    { # > figlet -f slant MCLUST
+      packageStartupMessage(
+"    __  ___________    __  _____________
+   /  |/  / ____/ /   / / / / ___/_  __/
+  / /|_/ / /   / /   / / / /\\__ \\ / /   
+ / /  / / /___/ /___/ /_/ /___/ // /    
+/_/  /_/\\____/_____/\\____//____//_/    version ", version)
+}
+else
+  { packageStartupMessage("Package 'sFPCA' version ", version) } 
+
+  invisible()
+}
+
